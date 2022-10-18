f5b47ae131dcc7c679017a1da476daa12ae75a39
Richard Mands <richard@cinderhousecreative.com>
2022-10-18 15:30:23 +0900

    Google Tag Manager with Popup for Cookie

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,19 +3,29 @@ import Document, { Html, Head, Main, NextScript } from 'next/document'
 const AnalyticsScripts = () => {
   if (process.env.NODE_ENV !== 'production') return null
   return (
-    <>
-      <script async src="https://www.googletagmanager.com/gtag/js?id=G-Q3CT19D49H" />
-      <script
-        dangerouslySetInnerHTML={{
-          __html: `
-                window.dataLayer = window.dataLayer || [];
-                function gtag(){dataLayer.push(arguments);}
-                gtag('js', new Date());
-                gtag('config', 'G-Q3CT19D49H');
-              `,
-        }}
-      />
-    </>
+    <script
+      dangerouslySetInnerHTML={{
+        __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-WTZFRN7')`,
+      }}
+    />
+  )
+}
+
+const AnalyticsPopup = () => {
+  if (process.env.NODE_ENV !== 'production') return null
+  return (
+    <noscript>
+      <iframe
+        src="https://www.googletagmanager.com/ns.html?id=GTM-WTZFRN7"
+        height="0"
+        width="0"
+        style={{ display: 'none', visibility: 'hidden' }}
+      ></iframe>
+    </noscript>
   )
 }
 
@@ -65,13 +75,13 @@ class MyDocument extends Document {
           `,
             }}
           />
-
           <AnalyticsScripts />
         </Head>
         <body>
           <Main />
           <NextScript />
         </body>
+        <AnalyticsPopup />
       </Html>
     )
   }
