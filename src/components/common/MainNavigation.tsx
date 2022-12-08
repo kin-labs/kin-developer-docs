2e1089d08f5b76307d414d73ed49fcb5d2fbbfa4
Richard Mands <richard@cinderhousecreative.com>
2022-12-08 10:41:06 +0900

    Kin Tools init

--- src/components/common/MainNavigation.tsx
+++ src/components/common/MainNavigation.tsx
@@ -15,7 +15,7 @@ const navLinks: Array<{ label: string; url: string; hideLg?: boolean }> = [
   { label: 'Non-Developers', url: '/docs/non-developers' },
   { label: 'Kinetic', url: '/docs/kinetic', hideLg: true },
   { label: 'Integrations', url: '/docs/integrations', hideLg: true },
-  { label: 'Examples', url: '/examples', hideLg: true },
+  { label: 'Kin Tools', url: '/tools', hideLg: true },
 ]
 
 const iconLinks: Array<{ label: string; icon: IconName; url: string }> = [
--- src/contentlayer/index.ts
+++ src/contentlayer/index.ts
@@ -1,3 +1,4 @@
 export { Doc } from './document/Doc'
+export { Tool } from './document/Tool'
 export { Example } from './document/Example'
 export { GlobalConfig } from './document/GlobalConfig'
